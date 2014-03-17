81a0123535dc0b1b2328cb6e97c9f32a4a79ce48
Tanay Jaipuria <tanay46@hotmail.com>
2014-03-17 17:44:06 -0400

    Added NFC Toggle

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,6 @@
 var nfcdom = window.navigator.mozNfc;
 var nfc_status = document.getElementById("nfc_status");
+NFCToggle(true);
  
 document.getElementById('update_screen').onclick = function() {
     nfcdom = window.navigator.mozNfc;
@@ -12,3 +13,32 @@ document.getElementById('update_screen').onclick = function() {
 	nfc_status.innerHTML = "NFC DISABLED!!";
     }
 };
+
+function NFCToggle(enabled) {
+	ok(settings instanceof SettingsManager, 'settings instanceof ' + settings.constructor + ', expected SettingsManager');
+	let req = settings.createLock().get('nfc.enabled');
+	if req.onsuccess = function() {
+		if (req.result['nfc.enabled'] === enabled) {
+			callback();
+		} else {
+	    	let req = settings.createLock().set({'nfc.enabled': enabled});
+	    	req.onsuccess = function() {
+		    	window.setTimeout(callback, 5000); // give emulator time to toggle NFC
+			};
+			req.onerror = function() {
+		        ok(false,
+		           'Setting \'nfc.enabled\' to \'' + enabled +
+		           '\' failed, error ' + req.error.name);
+		        finish();
+		      };
+		 }
+	};
+	req.onerror = function() {
+	    ok(false, 'Getting \'nfc.enabled\' failed, error ' + req.error.name);
+	    finish();
+	  };
+	}
+
+function getDeviceStorage() {
+	var appstorage = window.navigator.getDeviceStorage("apps");
+}
